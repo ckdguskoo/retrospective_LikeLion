6b286f62a34f6796b71f00357b2c30e61735a09e
koochanghyen <kuku0746@naver.com>
2025-09-18 19:43:16 +0900

    생성자와 This

---
+++ src/day11/thisExam.java
@@ -0,0 +1,52 @@
+package day11;
+
+public class thisExam {
+    //[도서 관리 시스템] 의 Book 클래스를 만들고 Book 객체를 생성해 보세요.
+    //(Book 객체를 생성할 때, Book 의 필드 값도 가지고 생성될 수 있도록 고민해 보세요.)
+//    static class Book{
+//        String title;
+//        String author;
+//        String publishYear;
+//        String Isbn;
+//
+//        public Book() {
+//            this.title = title;
+//            this.author = author;
+//            this.publishYear = publishYear;
+//            this.Isbn = Isbn;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Book book = new Book();
+//        book.Isbn = "12345";
+//        System.out.println(book.Isbn);
+//    }
+
+
+    //[학생 성적 관리 시스템]의 Student 클래스를 만들어보세요.
+    //(Student 객체를 생성할 때, Student 의 필드 값도 가지고 생성될 수 있도록 고민해 보세요.)
+    //
+    //
+    //필드:
+    //
+    //- 이름(name)
+    //- 학번(studentId)
+    //- 국어점수(koreanScore)
+    //- 영어점수(englishScore)
+    //- 수학점수(mathScore)
+    public class Student{
+        String name;
+        int koreanScore;
+        int englishScore;
+        int mathScore;
+    }
+
+
+
+
+
+
+
+
+}
